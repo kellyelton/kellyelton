8566c4746260ab4ff6bd0d44ca7c24a2901b6c4a
Kelly Elton <its.the.doc@gmail.com>
2017-06-05 16:53:55 -0400

    Update logging and add signaling

---
+++ src/KellyElton/ISignaler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace KellyElton
+{
+    public interface ISignaler
+    {
+        void Event(string type, int severity, object[] attachements = null);
+    }
+
+    public static class Signal
+    {
+        public static ISignaler DefaultSignaler { get; set; }
+
+        public static void Exception(Exception ex, Severity severity = default(Severity)) {
+            Event(nameof(Exception), severity, ex);
+        }
+
+        public static void Event(string type, Severity severity = Severity.Not, params object[] attachements) {
+            DefaultSignaler?.Event(type, (int)severity, attachements);
+        }
+    }
+
+    public enum Severity
+    {
+        Not = -1,
+        Normal = 0,
+        High = 1,
+        Critical = 2
+    }
+}
